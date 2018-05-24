ddbf6ff0b290ab63f37d457c3cc2980c43adb330
crownbackend <crownbackend@gmail.com>
2018-05-24 14:39:42 +0200

    debut du projet

---
+++ src/Controller/FrontController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FrontController extends Controller {
+
+    /**
+     * @Route("/")
+     * @return Response
+     */
+
+    public function index(): Response {
+
+        return $this->render('index.html.twig', [
+            'title' => 'Bienvenue sur mon cpf.info, le site de la formation professionnelle ',
+            'description' => 'Venez faire des formations chez nous grace a votre compte CPF c\'est gratuit',
+            'keywords' => 'formation professionnelle, formation pro, formation salariés, formation adultes, formation dif, formation cif, formation vae, formation CPF'
+        ]);
+
+    }
+
+
+    /**
+     * @Route("/etapes-a-suivre")
+     * @return Response
+     */
+    public function etapeASuivre(): Response {
+
+        return $this->render('etape_a_suivre.html.twig', [
+            'title' => 'Étape a suivre',
+            'description' => 'Retrouvez ci-dessous les différentes étapes à suivre pour s\'inscrire à la formation souhaitée.',
+            'keywords' => 'Mon compte, CPF, inscription, formation'
+        ]);
+    }
+
+    /**
+     * @Route("/les-formations")
+     * @return Response
+     */
+
+    public function formations(): Response {
+
+        return $this->render('formations.html.twig', [
+            'title' => 'Tout nos formations',
+            'description' => 'Retrouvez ici tout nos formations Achat, Bureautique, Commerce et bien plus encore',
+            'keywords' => 'Mon compte, CPF, inscription, formation, anglais, VTC, Assistanat, Secrétariat, Support'
+        ]);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
